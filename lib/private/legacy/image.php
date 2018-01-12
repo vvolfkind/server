cdaa85d7236dfa14b3eaa2d2a6d9c1080c1802ca
Morris Jobke <hey@morrisjobke.de>
2018-01-13 00:34:28 +0100

    Fix float/integer handling in image API
    * IImage::crop/preciseResize now have type hinting for integers
    * found while testing strict typing for PHP 7+
    
    Signed-off-by: Morris Jobke <hey@morrisjobke.de>

--- lib/private/legacy/image.php
+++ lib/private/legacy/image.php
@@ -550,6 +550,13 @@ class OC_Image implements \OCP\IImage {
 	 * @return bool|resource An image resource or false on error
 	 */
 	public function loadFromFile($imagePath = false) {
+		try {
+			// detect if it is a path or maybe the images as string
+			// needed because the constructor iterates over all load* methods
+			realpath($imagePath);
+		} catch (TypeError $e) {
+			return false;
+		}
 		// exif_imagetype throws "read error!" if file is less than 12 byte
 		if (!@is_file($imagePath) || !file_exists($imagePath) || filesize($imagePath) < 12 || !is_readable($imagePath)) {
 			return false;
@@ -873,7 +880,7 @@ class OC_Image implements \OCP\IImage {
 			$newHeight = $maxSize;
 		}
 
-		$this->preciseResize(round($newWidth), round($newHeight));
+		$this->preciseResize((int)round($newWidth), (int)round($newHeight));
 		return true;
 	}
 
@@ -882,7 +889,7 @@ class OC_Image implements \OCP\IImage {
 	 * @param int $height
 	 * @return bool
 	 */
-	public function preciseResize($width, $height) {
+	public function preciseResize(int $width, int $height): bool {
 		if (!$this->valid()) {
 			$this->logger->error(__METHOD__ . '(): No image loaded', array('app' => 'core'));
 			return false;
@@ -982,7 +989,7 @@ class OC_Image implements \OCP\IImage {
 	 * @param int $h Height
 	 * @return bool for success or failure
 	 */
-	public function crop($x, $y, $w, $h) {
+	public function crop(int $x, int $y, int $w, int $h): bool {
 		if (!$this->valid()) {
 			$this->logger->error(__METHOD__ . '(): No image loaded', array('app' => 'core'));
 			return false;
@@ -1033,7 +1040,7 @@ class OC_Image implements \OCP\IImage {
 		$newWidth = min($maxWidth, $ratio * $maxHeight);
 		$newHeight = min($maxHeight, $maxWidth / $ratio);
 
-		$this->preciseResize(round($newWidth), round($newHeight));
+		$this->preciseResize((int)round($newWidth), (int)round($newHeight));
 		return true;
 	}
 
